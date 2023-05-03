7863fda806bc5132fc20296ac01795c356be0a85
Eilifirn <132464090+Eilifirn@users.noreply.github.com>
2023-05-03 17:09:01 +0300

    Update token.js

--- src/token.js
+++ src/token.js
@@ -1,3 +1,7 @@
 window.onload = () => {
- YaSendSuggestToken("https://oauth-master-class-azfqrcjmj-eilifirn.vercel.app"); // TODO
+ YaSendSuggestToken(
+   'https://oauth-master-class-azfqrcjmj-eilifirn.vercel.app', {
+      flag: true
+   }
+) // TODO
 };
